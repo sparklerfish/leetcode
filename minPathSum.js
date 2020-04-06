a11d57c0d980ab9cce188f28282435c7d52585d8
Julie Adams <julie.ellen.adams@gmail.com>
2020-04-05 21:14:38 -0700

    Solve Minimum Path Sum

--- minPathSum.js
+++ minPathSum.js
@@ -1,10 +1,25 @@
 function minPathSum(grid) {
-    let table = new Array(grid.length);
-    for (let i = 0; i < grid.length; i++) {
-        table[i] = new Array(grid[0].length).fill(Infinity);
-    }
+    let m = grid.length;
+    let n = grid[0].length;
 
-    console.log(table);
+    let table = new Array(m).fill().map(() => new Array(n).fill(Infinity));
+
+    table[0][0] = grid[0][0];
+
+    // console.log(table);
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (j < n - 1) {
+                table[i][j + 1] = Math.min(table[i][j] + grid[i][j + 1], table[i][j + 1])
+            }
+            if (i < m - 1) {
+                table[i + 1][j] = Math.min(table[i][j] + grid[i + 1][j], table[i + 1][j])
+            }
+        }
+    }
+    console.log(table[m - 1][n - 1]);
+    return table[m - 1][n -1];
 }
 
 minPathSum([
