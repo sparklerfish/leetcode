bddb8dca1a24bca7354835527f23c961f70d56e2
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-05 21:19:26 -0700

    Start JS solution for Spiral Traverse

---
+++ spiral_traverse.js
@@ -0,0 +1,13 @@
+function spiralTraverse(array) {
+  // Write your code here.
+	let width = array[0].length;
+	let height = array.length;
+	
+	let visited = new Set();
+	
+	let x = 0;
+	let y = 0;
+	
+	
+	
+}
