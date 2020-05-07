63a8ef1f134d1c1fc0c201733df0ee71ea2ced78
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-06 18:48:09 -0700

    Longest Peak JS solution

---
+++ longest_peak.js
@@ -0,0 +1,28 @@
+function longestPeak(array) {
+  // Write your code here.
+	let streak = 0;
+	let longest = 0;
+	
+	for (let i = 1; i < array.length - 1; i++) {
+		if (array[i - 1] < array[i] && array[i] > array[i + 1]) {
+			streak = 1;
+			let peak = i;
+			
+			while (array[i - 1] < array[i]) {
+				streak++;
+				i--;
+			}
+			
+			i = peak;
+			while (array[i] > array[i + 1]) {
+				streak++;
+				i++;
+			}
+			
+			
+			if (streak > longest) longest = streak;
+		}
+	}
+	
+	return longest;
+}
