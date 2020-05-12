77abffd9e98b96e7fb8827db1c7be854fb2e2c32
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-12 12:39:51 -0700

    Single non duplicate JS solution

---
+++ single_non_duplicate.js
@@ -0,0 +1,13 @@
+var singleNonDuplicate = function(nums) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    
+    while (start < end) {
+        let mid = (start + end) / 2;
+        if (mid % 2 === 1) mid--;
+        (nums[mid] === nums[mid + 1]) ? start = mid + 2 : end = mid;
+    }
+
+    return nums[start];
+};
