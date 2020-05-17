1d71b7a86221049e1342d0b1b0196add2fbb4f6b
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-16 21:15:55 -0700

    Odd Even Linked List JS Solution

---
+++ odd_even_list.js
@@ -0,0 +1,17 @@
+var oddEvenList = function(head) {
+    if (!head) return head;
+    
+    let oddNode = head;
+    let evenHead = head.next;
+    let evenNode = evenHead;
+    
+    while (oddNode.next && oddNode.next.next) {
+        oddNode.next = oddNode.next.next;
+        evenNode.next = evenNode.next.next;
+        oddNode = oddNode.next;
+        evenNode = evenNode.next;
+    }
+    
+    oddNode.next = evenHead;
+    return head;
+};
