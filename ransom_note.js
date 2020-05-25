c051753469c64c2ae0e56c52e423ff9b4b65c18c
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-24 23:52:58 -0700

    Fix formatting

--- ransom_note.js
+++ ransom_note.js
@@ -1,14 +1,6 @@
 var canConstruct = function(ransomNote, magazine) {
-//     const sortedNote = ransomNote.split("").sort();
-//     const sortedMag = magazine.split("").sort();
-    
-//     while(sortedMag.length > 0) {
-//         if (sortedNote[0] === sortedMag[0]) sortedNote.shift();
-//         sortedMag.shift();
-//     }
-    
-//     return sortedNote.length === 0;
     const letters = {};
+    
     for (let i = 0; i < magazine.length; i++) {
         let char1 = magazine[i];
         letters[char1] ? letters[char1] += 1 : letters[char1] = 1
